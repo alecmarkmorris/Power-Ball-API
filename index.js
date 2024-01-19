df0a490452666a226367db2e8963142af445c59f
alecmarkmorris <Alecmarkmorris@gmail.com>
2024-01-19 11:03:04 -0700

    delete

--- index.js
+++ index.js
@@ -50,31 +50,8 @@ app.get('/:yearId', (req, res) => {
 })
 
 app.get('/sendemail', (req, res) => {
-    async function sendEmail() {
-        try {
-          const transporter = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-              user: 'alecmarkmorris@gmail.com',
-              pass: 'hnebxzalnffmitav'
-            }
-          });
-      
-          const mailOptions = {
-            from: 'alecmarkmorris@gmail.com',
-            to: 'alecmarkmorris@example.com',
-            subject: 'Hello',
-            text: 'This is the body of the email.'
-          };
-      
-          const info = await transporter.sendMail(mailOptions);
-          console.log('Email sent:', info.messageId);
-        } catch (error) {
-          console.error('Error occurred:', error);
-        }
-      }
-      
-      sendEmail();
+    res.json('This will display alk my send email')
+
 })
 
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
